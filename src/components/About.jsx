18adda2833f4741bd8aa3a588bb6e7645ac442eb
Pengwius <tomekftomekf@gmail.com>
2024-01-05 00:23:32 +0100

    Fixed tiles view

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,32 +3,34 @@ import React from 'react';
 export const About = () => {
   return (
     <section className="about" id="about">
-      <div className="tiles">
-        <div className="tiles-row">
-          <div className="tile">
-            <div className="tile-content">
-              <p className="tile-title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quas.</p>
-              <p className="tile-subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quas.</p>
+      <div className="container z-3">
+        <div className="tiles">
+          <div className="tiles-row">
+            <div className="tile">
+              <div className="tile-content">
+                <p className="tile-title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quas.</p>
+                <p className="tile-subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quas.</p>
+              </div>
+            </div>
+            <div className="tile">
+              <div className="tile-content">
+                <p className="tile-title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quas.</p>
+                <p className="tile-subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quas.</p>
+              </div>
             </div>
           </div>
-          <div className="tile">
-            <div className="tile-content">
-              <p className="tile-title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quas.</p>
-              <p className="tile-subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quas.</p>
+          <div className="tiles-row">
+            <div className="tile">
+              <div className="tile-content">
+                <p className="tile-title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quas.</p>
+                <p className="tile-subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quas.</p>
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="tiles-row">
-          <div className="tile">
-            <div className="tile-content">
-              <p className="tile-title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quas.</p>
-              <p className="tile-subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quas.</p>
-            </div>
-          </div>
-          <div className="tile">
-            <div className="tile-content">
-              <p className="tile-title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quas.</p>
-              <p className="tile-subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quas.</p>
+            <div className="tile">
+              <div className="tile-content">
+                <p className="tile-title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quas.</p>
+                <p className="tile-subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quas.</p>
+              </div>
             </div>
           </div>
         </div>
